dcf43acdc4df2b31e0f3f06fcc57c59fe011a765
eddieblackadder <recruitlukejones@gmail.com>
2020-06-30 13:20:26 +0100

    tests file

---
+++ tests/movies_list_model.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+import Model from '../assets/js/movies_list/model.js'
+
+describe('Model', function () {
+  const model = new Model();
+  const API_KEY = 'APY_KEY';
+
+  
+});
